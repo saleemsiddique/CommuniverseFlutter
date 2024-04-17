cc89dc90b236924014e2086c7299bf758b695bf7
saleemsiddique <saleemprogramacion04@gmail.com>
2024-04-17 10:52:04 +0200

    Follow service upgrade

--- communiverse/lib/services/user_service.dart
+++ communiverse/lib/services/user_service.dart
@@ -14,86 +14,89 @@ class UserService extends ChangeNotifier {
   User searchedUser = new User.empty();
   List<User> searchedUsersList = [];
 
-
-signUp(Map<String, dynamic> data) async {
-  try {
-    final jsonData = await CommuniverseProvider.postJsonData(
-        '${CommuniverseProvider.apiAuthRoutes}signup', data);
-    user = User.fromJson(json.decode(jsonData));
-    print("Este es el usuario creado: $user");
-    notifyListeners();
-  } catch (error) {
-    // Extraer el mensaje de error de la cadena de excepción
-    String errorMessage = error.toString().replaceAll('Exception: ', '');
-    throw errorMessage;
+  signUp(Map<String, dynamic> data) async {
+    try {
+      final jsonData = await CommuniverseProvider.postJsonData(
+          '${CommuniverseProvider.apiAuthRoutes}signup', data);
+      user = User.fromJson(json.decode(jsonData));
+      print("Este es el usuario creado: $user");
+      notifyListeners();
+    } catch (error) {
+      // Extraer el mensaje de error de la cadena de excepción
+      String errorMessage = error.toString().replaceAll('Exception: ', '');
+      throw errorMessage;
+    }
   }
-}
 
-findUserById(String id) async {
-  try {
-    final jsonData = await CommuniverseProvider.getJsonData('user/${id}');
-    user = User.fromJson(json.decode(jsonData));
-    print("Este es el usuario encotrado por ID: $user");
-    notifyListeners();
-  } catch (error) {
-    String errorMessage = error.toString().replaceAll('Exception: ', '');
-    throw errorMessage;
+  findUserById(String id) async {
+    try {
+      final jsonData = await CommuniverseProvider.getJsonData('user/${id}');
+      user = User.fromJson(json.decode(jsonData));
+      print("Este es el usuario encotrado por ID: $user");
+      notifyListeners();
+    } catch (error) {
+      String errorMessage = error.toString().replaceAll('Exception: ', '');
+      throw errorMessage;
+    }
   }
-}
 
-searchOtherUsers(String username) async {
-  try {
-    final jsonData = await CommuniverseProvider.getJsonData('user/username/${username}');
-    searchedUser = User.fromJson(json.decode(jsonData));
-    print("Este es el usuario encotrado por ID: $user");
-    notifyListeners();
-  } catch (error) {
-    String errorMessage = error.toString().replaceAll('Exception: ', '');
-    throw errorMessage;
+  searchOtherUsers(String username) async {
+    try {
+      final jsonData =
+          await CommuniverseProvider.getJsonData('user/username/${username}');
+      searchedUser = User.fromJson(json.decode(jsonData));
+      print("Este es el usuario encotrado por ID: $user");
+      notifyListeners();
+    } catch (error) {
+      String errorMessage = error.toString().replaceAll('Exception: ', '');
+      throw errorMessage;
+    }
   }
-}
 
-searchUsersList(String username) async {
-  try {
-    final jsonData = await CommuniverseProvider.getJsonData('user/usernameRegex/${username}');
-    final List<dynamic> jsonResponse = json.decode(jsonData);
-    searchedUsersList = jsonResponse.map((json) => User.fromJson(json)).toList();
-    print("Usuarios encontrados $searchedUsersList");
-    notifyListeners();
-  } catch (error) {
-    String errorMessage = error.toString().replaceAll('Exception: ', '');
-    throw errorMessage;
+  searchUsersList(String username) async {
+    try {
+      final jsonData = await CommuniverseProvider.getJsonData(
+          'user/usernameRegex/${username}');
+      final List<dynamic> jsonResponse = json.decode(jsonData);
+      searchedUsersList =
+          jsonResponse.map((json) => User.fromJson(json)).toList();
+      print("Usuarios encontrados $searchedUsersList");
+      notifyListeners();
+    } catch (error) {
+      String errorMessage = error.toString().replaceAll('Exception: ', '');
+      throw errorMessage;
+    }
   }
-}
 
-Future<User> findById(String id) async {
-  try {
-    final jsonData = await CommuniverseProvider.getJsonData('user/${id}');
-    User user = User.fromJson(json.decode(jsonData));
-    print("Este es el usuario encontrado por ID: $user");
-    return user;
-  } catch (error) {
-    String errorMessage = error.toString().replaceAll('Exception: ', '');
-    throw errorMessage;
+  Future<User> findById(String id) async {
+    try {
+      final jsonData = await CommuniverseProvider.getJsonData('user/${id}');
+      User user = User.fromJson(json.decode(jsonData));
+      print("Este es el usuario encontrado por ID: $user");
+      return user;
+    } catch (error) {
+      String errorMessage = error.toString().replaceAll('Exception: ', '');
+      throw errorMessage;
+    }
   }
-}
 
-follow(String idFollowing, String idFollowed) async {
-  try {
-    await CommuniverseProvider.getJsonData('user/follow/${idFollowing}/${idFollowed}');
-    print('$idFollowing has started following $idFollowed');
-    // Obtener el usuario seguido por su id
-    User followedUser = await findById(idFollowed);
-    // Buscar otros usuarios relacionados con el usuario seguido
-    await searchOtherUsers(followedUser.username);
-    // Notificar a los listeners después de completar todas las operaciones
-    notifyListeners();
-  } catch (error) {
-    String errorMessage = error.toString().replaceAll('Exception: ', '');
-    throw errorMessage;
+  follow(String idFollowing, String idFollowed) async {
+    try {
+      final jsonData = await CommuniverseProvider.getJsonData(
+          'user/follow/${idFollowing}/${idFollowed}');
+      searchedUser = User.fromJson(json.decode(jsonData));
+      print('$idFollowing has started following $idFollowed');
+      // Obtener el usuario seguido por su id
+      // User followedUser = await findById(idFollowed);
+      // Buscar otros usuarios relacionados con el usuario seguido
+      // await searchOtherUsers(followedUser.username);
+      // Notificar a los listeners después de completar todas las operaciones
+      notifyListeners();
+    } catch (error) {
+      String errorMessage = error.toString().replaceAll('Exception: ', '');
+      throw errorMessage;
+    }
   }
-}
-
 
   void clearData() {
     formKey = new GlobalKey<FormState>();
